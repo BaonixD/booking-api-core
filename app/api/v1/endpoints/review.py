b18cc21af94d8bda4ea91bbeb4e055bdea2ba946
Ilyas <amzehanilias@gmail.com>
2026-01-23 00:29:27 +0500

    .

--- app/api/v1/endpoints/review.py
+++ app/api/v1/endpoints/review.py
@@ -5,9 +5,8 @@ from typing import List
 from app.db.session import get_db
 from app.schemas.review import ReviewCreate, ReviewResponse, ReviewUpdate
 from app.crud import review as crud_review
-from app.crud.user import get_current_user  # Твоя готовая функция
-from app.models.User import User  # Импорт модели юзера для аннотации типов
-
+from app.crud.user import get_current_user
+from app.models.User import User
 router = APIRouter(prefix="/reviews", tags=["Reviews"])
 
 
@@ -15,9 +14,9 @@ router = APIRouter(prefix="/reviews", tags=["Reviews"])
 async def create_my_review(
         review_in: ReviewCreate,
         db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user)  # Сами берем юзера из токена
+        current_user: User = Depends(get_current_user)
 ):
-    # user_id берем из объекта current_user, который вернула твоя функция
+
     return await crud_review.create_review(db, review_in, user_id=current_user.id)
 
 
